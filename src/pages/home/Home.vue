bc722c34ceee3e89a8893f67155540b42b05ff8f
luoyupiaoshang@gmail.com <15733207536@163.com>
2018-11-19 15:55:51 +0800

    perf(Home.vue): import Header component

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,9 +1,14 @@
 <template lang="pug">
- .container hello
+  .container
+    home-header
 </template>
 <script type="text/ecmascript-6">
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 <style lang="postcss" scoped>
