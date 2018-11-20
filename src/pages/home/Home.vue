bf960f0b057bfe798a3866344d13292a1f3d12e1
luoyupiaoshang@gmail.com <15733207536@163.com>
2018-11-20 18:12:58 +0800

    perf(Home): import recommend & weekend component

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,17 +3,23 @@
     home-header
     home-swiper
     home-icons
+    home-recommend
+    home-weekend
 </template>
 <script type="text/ecmascript-6">
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
