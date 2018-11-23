b8b8660fafe97983d822ff0f5424f521964df1c5
luoyupiaoshang@gmail.com <15733207536@163.com>
2018-11-23 23:52:12 +0800

    refactor(vue.config.js): use jsonServer API

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,26 @@
 const path = require('path')
+
 function resolve (dir) {
   return path.join(__dirname, dir)
 }
+
 module.exports = {
   lintOnSave: true,
   chainWebpack: (config) => {
     config.resolve.alias
       .set('@', resolve('src'))
       .set('styles', resolve('src/assets/styles'))
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/api/',
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
   }
 }
