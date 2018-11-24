344fcf07d0379b36579fb020984fcf732446fef1
luoyupiaoshang@gmail.com <15733207536@163.com>
2018-11-24 22:53:37 +0800

    feat(City.vue): + City router

---
+++ src/pages/city/City.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    .container
+      city-header
+</template>
+<script type="text/ecmascript-6">
+import CityHeader from './components/Header'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+<style lang="postcss" scoped>
+
+</style>
