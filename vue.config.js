7f57cb76fb4ca8d8a82dee7a9afc917d7e2c1e18
lovemoganna <15733207536@163.com>
2018-11-27 22:59:22 +0800

    feat(Vue.config): + common component alais config

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,7 @@ module.exports = {
     config.resolve.alias
       .set('@', resolve('src'))
       .set('styles', resolve('src/assets/styles'))
+      .set('common', resolve('src/common'))
   },
   devServer: {
     proxy: {
