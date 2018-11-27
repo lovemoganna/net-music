9ae14c196528e37815b655f0c55aa54be950125b
lovemoganna <15733207536@163.com>
2018-11-27 22:57:24 +0800

    feat(Common): + common gallary components

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="item in imgs" :key="item.id">
+          <img class="swiper-image"
+               :src="item"
+               alt="">
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+
+</template>
+<script type="text/ecmascript-6">
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction'
+        },
+        // 只要swiper组件监听到父级元素的DOM发生变化的时候,会自动刷新一次,。
+        observer: true,
+        observeParents: true
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .container {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background: #000;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    .wrapper {
+      overflow: inherit;
+      height: 0;
+      padding-bottom: 68.57%;
+      width: 100%;
+      .swiper-container {
+        overflow: inherit;
+        .swiper-img {
+          width: 100%;
+        }
+        .swiper-pagination {
+          color: #ffffff;
+          bottom: -1rem;
+        }
+      }
+    }
+  }
+
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,81 @@
+<template lang="pug">
+  .banner
+    img.banner-img(src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg")
+    .banner-bar
+      .banner-icons
+        span.iconfont.banner-icon &#xe736;
+        e.banner-icon-number 10
+      .banner-info
+        .banner-title 故宫(AAAAA景区)
+    .banner-back
+      span.iconfont.banner-icon-back &#xe660;
+</template>
+<script type="text/ecmascript-6">
+export default {
+  name: 'DetailHeader'
+}
+</script>
+<style lang="scss" scoped>
+  .banner {
+    position: relative;
+    overflow: hidden;
+    height: 0;
+    padding-bottom: 55%;
+    .banner-img {
+      width: 100%;
+    }
+    .banner-bar {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: .2rem;
+      line-height: .6rem;
+      color: white;
+      .banner-icons {
+        width: 1.2rem;
+        margin-left: .2rem;
+        height: .4rem;
+        line-height: .4rem;
+        background: rgba(0, 0, 0, .5);
+        border-radius: .2rem;
+        color: #fff;
+        text-align: center;
+        font-size: .24rem;
+        background-image: linear-gradient(to top,rgba(0,0,0,0),rgba(0,0,0,0.8));
+        .banner-icon {
+          margin-right: .1rem;
+          font-size: .24rem;
+        }
+        .banner.icon-number {
+          margin-left: .1rem;
+        }
+      }
+      .banner-info {
+        .banner-title {
+          margin-left: .2rem;
+          color: #fff;
+          font-size: .36rem;
+          text-shadow: 0 1px 2px rgba(0, 0, 0, 0.70);
+        }
+      }
+    }
+    .banner-back{
+      position: absolute;
+      top: .1rem;
+      left: .1rem;
+      width: .72rem;
+      height: .72rem;
+      line-height: .72rem;
+      background: rgba(0, 0, 0, 0.66);
+      border-radius: .45rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .banner-icon-back{
+        font-size: .36rem;
+        color: #ffffff;
+      }
+    }
+  }
+
+</style>
