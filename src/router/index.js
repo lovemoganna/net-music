fc5c4158a5ceed8dcd4ae54dddcfbe99da76c5c0
luoyupiaoshang@gmail.com <15733207536@163.com>
2018-12-04 15:50:29 +0800

    refator(Router)

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import City from '@/pages/city/City'
 import Detail from '@/pages/detail/Detail'
-import BMap from '@/test/bmap/BMap'
 
 Vue.use(Router)
 
@@ -25,11 +24,6 @@ export default new Router({
       path: '/detail/:id',
       name: 'Detail',
       component: Detail
-    },
-    {
-      path: '/bmap',
-      name: 'BMap',
-      component: BMap
     }
   ],
   scrollBehavior (to, from, savedPosition) {
